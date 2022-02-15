d64f34c73c460590dd7571757a5ef8849ad4a856
Bhavin <contact2bhavin@gmail.com>
2022-02-15 16:39:27 -0500

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+/*
+C:\Users\conta\OneDrive\Documents\Tech\Java>javac HelloWorld.java
+
+C:\Users\conta\OneDrive\Documents\Tech\Java>java HelloWorld.class
+Error: Could not find or load main class HelloWorld.class
+Caused by: java.lang.ClassNotFoundException: HelloWorld.class
+
+C:\Users\conta\OneDrive\Documents\Tech\Java>java HelloWorld
+Hello, World!
+
+*/
